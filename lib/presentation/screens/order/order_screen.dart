c0545f4f670b388bfabe41272e75973238f59f61
Lìlz ♡⚡︎ <liza@Lizas-Laptop.local>
2026-01-04 15:45:53 +0700

    configure merge

---
+++ lib/presentation/screens/order/order_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../../widgets/order_card.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text(
+          'My Orders',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: 5, // mock orders
+        itemBuilder: (context, index) {
+          return const OrderCard();
+        },
+      ),
+    );
+  }
+}
